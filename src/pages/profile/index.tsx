66e96db92001a9c94a3f042510e1bda7340097b9
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-12 06:48:10 +0100

    feat(Profile): get user firstName and display it ✨

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,22 @@
 import './index.scss'
+import Header from 'components/Header'
+import { useEffect, useState } from 'react'
+import { useApi } from 'utils/hooks'
+import { User } from 'utils/types'
 
-export default function Profile() {
-  return <main className="content">Profile</main>
+export default function Profile(): JSX.Element {
+  const [user, setUser] = useState<User>()
+  const data = useApi('/user/12')
+
+  useEffect(() => {
+    if (data) {
+      setUser(data)
+    }
+  }, [data])
+  console.log(user)
+  return (
+    <main className="content">
+      {user && <Header name={user.userInfos.firstName} />}
+    </main>
+  )
 }
