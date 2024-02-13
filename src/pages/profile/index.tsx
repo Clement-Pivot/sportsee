cc798a8f1796b08966fae52b989b050cc486bdd5
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-13 03:56:30 +0100

    feat(Profile): add ActivityGraph component ✨

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,21 +2,31 @@ import './index.scss'
 import Header from 'components/Header'
 import { useEffect, useState } from 'react'
 import { useApi } from 'utils/hooks'
-import { User } from 'utils/types'
+import { User, Activity } from 'utils/types'
+import ActivityGraph from 'components/ActivityGraph'
 
 export default function Profile(): JSX.Element {
   const [user, setUser] = useState<User>()
-  const data = useApi('/user/12')
+  const [activity, setActivity] = useState<Activity>()
+  const userApi = useApi('/user/12')
+  const activityApi = useApi('/user/12/activity')
 
   useEffect(() => {
-    if (data) {
-      setUser(data)
+    if (userApi) {
+      setUser(userApi as User)
     }
-  }, [data])
-  console.log(user)
+  }, [userApi])
+
+  useEffect(() => {
+    if (activityApi) {
+      setActivity(activityApi as Activity)
+    }
+  }, [activityApi])
+
   return (
     <main className="content">
       {user && <Header name={user.userInfos.firstName} />}
+      {activity && <ActivityGraph content={activity} />}
     </main>
   )
 }
