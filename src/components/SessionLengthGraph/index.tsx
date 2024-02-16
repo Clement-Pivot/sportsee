1962811a6a4a6541255c1e5f02b6cd4678e9927f
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-16 22:11:33 +0100

    feat: create SessionLengthGraph function ✨

--- src/components/SessionLengthGraph/index.tsx
+++ src/components/SessionLengthGraph/index.tsx
@@ -1,1 +1,10 @@
+import { AverageSessions } from 'utils/types'
 import './index.scss'
+
+type Props = {
+  content: AverageSessions
+}
+
+export default function SessionLengthGraph({ content }: Props) {
+  return <div className="sessionLengthGraph"></div>
+}
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,17 +2,20 @@ import './index.scss'
 import Header from 'layouts/Header'
 import { useEffect, useState } from 'react'
 import { useApi } from 'utils/hooks'
-import { User, Activity, Dimensions } from 'utils/types'
+import { User, Activity, Dimensions, AverageSessions } from 'utils/types'
 import { downScale } from 'utils/helpers'
 import ActivityGraph from 'components/ActivityGraph'
+import SessionLengthGraph from 'components/SessionLengthGraph'
 import { useParams } from 'react-router-dom'
 
 export default function Profile(): JSX.Element {
   const { id } = useParams()
   const [user, setUser] = useState<User>()
   const [activity, setActivity] = useState<Activity>()
+  const [averageSessions, setAverageSessions] = useState<AverageSessions>()
   const userApi = useApi(`/user/${id}`)
   const activityApi = useApi(`/user/${id}/activity`)
+  const averageSessionApi = useApi(`/user/${id}/average-sessions`)
   const activityDimensions: Dimensions = {
     width: downScale(835),
     height: downScale(320),
@@ -34,12 +37,19 @@ export default function Profile(): JSX.Element {
     }
   }, [activityApi])
 
+  useEffect(() => {
+    if (averageSessionApi) {
+      setAverageSessions(averageSessionApi as AverageSessions)
+    }
+  }, [averageSessionApi])
+
   return (
     <main className="content">
       {user && <Header name={user.userInfos.firstName} />}
       {activity && (
         <ActivityGraph content={activity} dimensions={activityDimensions} />
       )}
+      {averageSessions && <SessionLengthGraph content={averageSessions} />}
     </main>
   )
 }
