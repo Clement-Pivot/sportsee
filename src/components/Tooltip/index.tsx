aaabda63de2b0e83c232f90d7feab0f5c16ebf5a
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-17 02:35:52 +0100

    feat: created Tooltip component ✨

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,45 @@
+import './index.scss'
+import { useEffect } from 'react'
+
+type Props = {
+  content: {
+    key: any
+    props: {
+      children: Element
+    }
+    ref?: {
+      current: HTMLElement
+    }
+    type: string
+  }
+  subscribers: {
+    current: Element[]
+  }
+}
+
+export default function Tooltip({ content, subscribers }: Props) {
+  useEffect(() => {
+    function mouseOver(sub: any): void {
+      sub.style.opacity = '1'
+      content!.ref!.current!.style.display = 'flex'
+    }
+
+    function mouseMove(e: MouseEvent) {
+      content!.ref!.current!.style.left = `${e.pageX + 10}px`
+      content!.ref!.current!.style.top = `${e.pageY - 10}px`
+    }
+
+    function mouseLeave(sub: any) {
+      sub.style.opacity = '0'
+      content!.ref!.current!.style.display = 'none'
+    }
+
+    subscribers.current.forEach((sub) => {
+      sub.addEventListener('mouseover', () => mouseOver(sub))
+      sub.addEventListener('mousemove', (e) => mouseMove(e as any))
+      sub.addEventListener('mouseleave', () => mouseLeave(sub))
+    })
+  }, [])
+
+  return <>{content}</>
+}
