1e22db40ddeb216341ede991c4e5e367faa8bff6
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-02-28 23:35:33 +0100

    feat(TypeGraph): add Data type ✨

--- src/components/TypeGraph/index.tsx
+++ src/components/TypeGraph/index.tsx
@@ -8,9 +8,19 @@ type Props = {
   dimensions: Dimensions
 }
 
+type Data = {
+  axis: string
+  value: number
+}
+
 export default function TypeGraph({ content, dimensions }: Props) {
   const { width, height, marginTop, marginRight, marginBottom, marginLeft } =
     dimensions
-
+  const data: Data[] = content.data.map((item) => {
+    return {
+      axis: content.kind[item.kind],
+      value: item.value,
+    }
+  })
   return <svg className="typegraph" width={width} height={height}></svg>
 }
