246a192df38069fafd38fc5e4661a9927ffcb029
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-05 04:21:13 +0100

    docs: add JSDoc for helpers 📚

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,3 +1,9 @@
+/**
+ * Calculate new length with proportion
+ * @param {number} length
+ * @param {number} [originalLength=1440]
+ * @returns {number}
+ */
 export function downScale(
   length: number,
   originalLength: number = 1440,
@@ -6,6 +12,13 @@ export function downScale(
   return (length * width) / originalLength
 }
 
+/**
+ * Create an array from range[0] to range[1]
+ * @param {number} min
+ * @param {number} max
+ * @param {number} [step=1]
+ * @returns {number[]}
+ */
 export function getRange(min: number, max: number, step: number = 1): number[] {
   const result: number[] = []
   for (let i = min; i <= max; i += step) {
