536cd2c844501ba2da3b81445d977d05d4c2a98c
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-06 00:53:16 +0100

    docs: add Consumption JSDoc 📚

--- src/components/Consumption/index.tsx
+++ src/components/Consumption/index.tsx
@@ -1,13 +1,26 @@
 import { FunctionComponent } from 'react'
 import './index.scss'
 
+/**
+ * Consumption prop type
+ * @typedef {Props}
+ */
 type Props = {
   Icon: FunctionComponent
   value: string
   type: string
 }
 
-export default function Consumption({ Icon, value, type }: Props) {
+/**
+ * Consumption function component
+ * @export
+ * @param {Props} param0
+ * @param {FunctionComponent} param0.Icon
+ * @param {string} param0.value
+ * @param {string} param0.type
+ * @returns {JSX.Element}
+ */
+export default function Consumption({ Icon, value, type }: Props): JSX.Element {
   return (
     <div className="consumption__item">
       <Icon />
