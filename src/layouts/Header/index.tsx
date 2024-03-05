4a703b18d575420477592d4d2a06ab9dcad1e095
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-06 00:57:46 +0100

    docs: add Header JSDoc 📚

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,9 +1,20 @@
 import './index.scss'
 
+/**
+ * Header prop type
+ * @typedef {Props}
+ */
 type Props = {
   name: string
 }
 
+/**
+ * Header function component
+ * @export
+ * @param {Props} param0
+ * @param {string} param0.name
+ * @returns {JSX.Element}
+ */
 export default function Header({ name }: Props): JSX.Element {
   return (
     <div className="header">
