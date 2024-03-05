493d661ace7bf88711e4af1072e5635939c6ed2e
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-06 00:58:45 +0100

    docs: add Logo JSdoc 📚

--- src/layouts/Logo/index.tsx
+++ src/layouts/Logo/index.tsx
@@ -1,6 +1,11 @@
 import './index.scss'
 import LogoSVG from '/src/assets/Logo.svg?react'
 
+/**
+ * Logo function component
+ * @export
+ * @returns {JSX.Element}
+ */
 export default function Logo(): JSX.Element {
   return (
     <div className="logo">
